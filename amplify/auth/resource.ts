c3532796a75107b4b6a63fe65546d98b5e3937e2
Steven Leija <stevenleija@gmail.com>
2024-05-08 15:54:27 -0500

    Adding test function

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,7 +5,12 @@ import { defineAuth } from '@aws-amplify/backend';
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
 export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
+    loginWith: {
+        email: true
+    },
+    multifactor: {
+        mode: 'REQUIRED',
+        totp: true,
+        sms: false
+    }
 });
