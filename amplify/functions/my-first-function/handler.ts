bfd344780e85b3e829f4ad76045ede68ba398ef7
Steven Leija <stevenleija@gmail.com>
2024-05-08 18:46:36 -0500

    removing auth

--- amplify/functions/my-first-function/handler.ts
+++ amplify/functions/my-first-function/handler.ts
@@ -1,6 +1,13 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { env } from '$amplify/env/my-first-function'; // the import is '$amplify/env/<function name>'
 
-export const handler = async (event: any) => {
+export const handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
+    console.log(event);
+    console.log(context);
     console.log(`Hello, ${ env.NAME }!`);
-    return 'Hello from my first function!';
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify('Hello from my first function!')
+    };
 };
