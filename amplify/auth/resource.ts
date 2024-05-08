21ac271b66683bf66ca1a5dc290d464f85f2d76a
Steven Leija <stevenleija@gmail.com>
2024-05-08 18:20:10 -0500

    removing auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,10 +1,10 @@
-import { defineAuth } from '@aws-amplify/backend';
+// import { defineAuth } from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-export const auth = defineAuth({
+/*export const auth = defineAuth({
     loginWith: {
         email: {
             verificationEmailStyle: 'LINK',
@@ -32,4 +32,4 @@ export const auth = defineAuth({
             required: true
         }
     }
-});
+});*/
