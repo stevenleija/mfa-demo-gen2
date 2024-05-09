e6d52ce6884e6cab577db488fcf03b079af14e7b
Steven Leija <stevenleija@gmail.com>
2024-05-09 08:52:14 -0500

    removing auth

--- amplify/functions/my-first-function/resource.ts
+++ amplify/functions/my-first-function/resource.ts
@@ -1,15 +1,13 @@
 import { defineFunction } from '@aws-amplify/backend';
 
-const functionProps = {
+export const myFirstFunction = defineFunction({
     name: 'my-first-function',
     entry: './handler.ts',
-    // memoryMB: 256,
-    // timeoutSeconds: 60,
-    // // runtime: 20,
+    memoryMB: 256,
+    timeoutSeconds: 60,
+    runtime: 20,
     // environment: {
     //     NAME: 'World',
     //     // 'API_ENDPOINT': process.env.API_ENDPOINT
     // }
-};
-
-export const myFirstFunction = defineFunction(functionProps);
+});
