50d4da75baa8ce6c4aad1d9929ec937d8431f9cc
Steven Leija <stevenleija@gmail.com>
2024-05-09 09:38:39 -0500

    Adding auth, data, lambda back in

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Amplify } from 'aws-amplify';
-import outputs from '../amplify_outputs_bak1.json';
+import outputs from '../amplify_outputs.json';
 import { ThemeProvider } from '@aws-amplify/ui-react';
 import theme from './theme.ts';
 
